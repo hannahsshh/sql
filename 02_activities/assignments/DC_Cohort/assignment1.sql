a9416e163956008119f908f41c603a3ed456f10a
hannahsshh <hannah.shusterhyman@mail.utoronto.ca>
2025-11-04 20:43:03 -0500

    Adding code for section 3. aggregate and temp table sections. No code added to section 4. date because we have not gone over that in class

--- 02_activities/assignments/DC_Cohort/assignment1.sql
+++ 02_activities/assignments/DC_Cohort/assignment1.sql
@@ -86,7 +86,9 @@ ORDER BY v.vendor_name, vba.market_date;
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
 
-
+SELECT vendor_id, COUNT(*) as times_booth_rented
+FROM vendor_booth_assignments
+GROUP BY vendor_id;
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
 sticker to everyone who has ever spent more than $2000 at the market. Write a query that generates a list 
@@ -94,7 +96,18 @@ of customers for them to give stickers to, sorted by last name, then first name.
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
 
+SELECT c.customer_last_name
+, c.customer_first_name
+, SUM(cp.quantity * cp.cost_to_customer_per_qty) as total_spend
 
+FROM customer_purchases as cp
+INNER JOIN customer as c 
+	ON c.customer_id = cp.customer_id
+	
+GROUP BY cp.customer_id
+HAVING total_spend >= 2000
+
+ORDER BY c.customer_last_name, c.customer_first_name;
 
 --Temp Table
 /* 1. Insert the original vendor table into a temp.new_vendor and then add a 10th vendor: 
@@ -108,6 +121,14 @@ When inserting the new vendor, you need to appropriately align the columns to be
 VALUES(col1,col2,col3,col4,col5) 
 */
 
+
+DROP TABLE IF EXISTS temp.new_vendor;
+CREATE TABLE temp.new_vendor AS
+SELECT *
+FROM vendor;
+
+INSERT INTO temp.new_vendor (vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
+VALUES (10, "Thomass Superfood Store", "Fresh Focused", "Thomas", "Rosenthal");
 
 
 -- Date
